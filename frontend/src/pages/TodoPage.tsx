a0767432c515fd49d58d88620acb174ab458695f
davidvchang <dvalenzuelachang@gmail.com>
2025-02-15 22:16:32 -0700

    Add: input add task and btnAdd

--- frontend/src/pages/TodoPage.tsx
+++ frontend/src/pages/TodoPage.tsx
@@ -10,6 +10,15 @@ const TodoPage:React.FC = () => {
         </div>
 
         <InputSearch/>
+
+        <div className='flex gap-3 w-full'>
+            <input type="text" name="" id="" className='px-10 py-2 font-light border border-slate-200 rounded' placeholder='Add a new task'/>
+            <button className=' w-fit h-fit px-5 py-2 bg-sky-600 text-white rounded-md hover:brightness-95 hover:transition duration-300 cursor-pointer'>Add</button>
+        </div>
+
+        <div className='flex flex-col'>
+
+        </div>
     </section>
   )
 }
