7a3d5c96d394ef63fce583305356fa56f21c1558
davidvchang <dvalenzuelachang@gmail.com>
2025-02-15 23:23:55 -0700

    Add: Login Page design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
+import Login from "./pages/Login"
 import TodoPage from "./pages/TodoPage"
 
 function App() {
 
   return (
-    <main className="w-full min-h-screen bg-slate-50 p-10">
-      <TodoPage/>
+    <main className="w-full h-[100vh] bg-slate-50 p-10">
+      {/* <TodoPage/> */}
+      <Login/>
     </main>
   )
 }
