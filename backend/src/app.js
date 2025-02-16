73b33869b3590156aac044adaedd5879d30df0de
davidvchang <dvalenzuelachang@gmail.com>
2025-02-16 00:07:44 -0700

    Add: app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,13 @@
 import express from 'express'
+import morgan from 'morgan'
 
 const app = express()
 
-app.use()
+app.set('port', process.env.PORT || 4000)
+
+app.use(morgan("dev"))
+app.use(express.json())
+
+app.use("/api/tasks")
+
+export default app
