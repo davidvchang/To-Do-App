4ceb6d2f82866c0a7dc4197130996fc5311a29bd
davidvchang <dvalenzuelachang@gmail.com>
2025-02-15 23:47:32 -0700

    Add: register page design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,22 @@
 import Login from "./pages/Login"
+import Register from "./pages/Register"
 import TodoPage from "./pages/TodoPage"
+import {Route, Routes, BrowserRouter} from 'react-router-dom'
 
 function App() {
 
   return (
     <main className="w-full h-[100vh] bg-slate-50 p-10">
       {/* <TodoPage/> */}
-      <Login/>
+  
+      <BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+        </Routes>
+      
+      </BrowserRouter>
     </main>
   )
 }
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ChevronLeft } from 'lucide-react';
 
 const Register:React.FC = () => {
   return (
@@ -9,25 +10,46 @@ const Register:React.FC = () => {
 
                 <div className='flex flex-col gap-3 pb-2'>
 
-                    <div className='flex '>
+                    <div className='flex gap-2'>
+                        <div className='flex flex-col'>
+                            <label htmlFor="name" className='text-sm'>Name</label>
+                            <input type="text" name="name" id="name"className='px-5 py-2 text-sm border border-slate-300 rounded-sm' required/>
+                        </div>
+
+                        <div className='flex flex-col'>
+                            <label htmlFor="last_name" className='text-sm'>Last Name</label>
+                            <input type="text" name="last_name" id="last_name"className='px-5 py-2 text-sm border border-slate-300 rounded-sm' required/>
+                        </div>
 
                     </div>
+
 
                     <div className='flex flex-col'>
                         <label htmlFor="email" className='text-sm'>Email</label>
                         <input type="email" name="email" id="email"className='px-5 py-2 text-sm border border-slate-300 rounded-sm' required/>
                     </div>
 
-                    <div className='flex flex-col'>
-                        <label htmlFor="password" className='text-sm'>Password</label>
-                        <input type="password" name="password" id="password"className='px-5 py-2 text-sm border border-slate-300 rounded-sm' required/>
+                    <div className='flex gap-2'>
+                        <div className='flex flex-col'>
+                            <label htmlFor="password" className='text-sm'>Password</label>
+                            <input type="password" name="password" id="password"className='px-5 py-2 text-sm border border-slate-300 rounded-sm' required/>
+                        </div>
+
+                        <div className='flex flex-col'>
+                            <label htmlFor="password2" className='text-sm'>Confirm Password</label>
+                            <input type="password" name="password2" id="password2"className='px-5 py-2 text-sm border border-slate-300 rounded-sm' required/>
+                        </div>
+
                     </div>
+
 
                 </div>
 
-                <button className='bg-sky-600 w-full h-fit py-2 rounded text-white hover:brightness-95 hover:transition duration-300 cursor-pointer'>Sign in</button>
+                <div className='flex gap-2 items-center'>
+                    <a href="/" className='w-fit h-full border border-slate-400 rounded hover:bg-sky-50 hover:transition duration-300'><ChevronLeft className='w-[38px] h-[38px]'/></a>
+                    <button className='bg-sky-600 w-full h-fit py-2 rounded text-white hover:brightness-95 hover:transition duration-300 cursor-pointer'>Register</button>
 
-                <a href="/register" className='text-sm text-sky-600 text-center hover:text-sky-700 hover:transition duration-300'>Don't have an account? Sign Up</a>
+                </div>
             </form>
         </div>
     </section>
