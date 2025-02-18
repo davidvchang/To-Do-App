9b4dcd6d99e07b1b031987714a43ae23dc88fe1f
davidvchang <dvalenzuelachang@gmail.com>
2025-02-18 15:43:19 -0700

    Add: installation jwt e implementation in rutes

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,8 @@ import morgan from 'morgan'
 import tasksRoutes from './routes/tasks.routes.js'
 import usersRoutes from './routes/users.routes.js'
 
+import verifyToken from './middlewares/verifyToken.js'
+
 const app = express()
 
 app.set('port', process.env.PORT || 4000)
@@ -11,7 +13,7 @@ app.set('port', process.env.PORT || 4000)
 app.use(morgan("dev"))
 app.use(express.json())
 
-app.use("/api/tasks", tasksRoutes)
+app.use("/api/tasks", verifyToken, tasksRoutes)
 app.use("/api/users", usersRoutes)
 
 export default app
